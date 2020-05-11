b7b4cb994338e475b992db92490d3c7b3d71eefa
Vincent Le Goff <vb.legoff@free.fr>
2020-05-11 18:24:27 +0200

    Ajout constructeurs

--- noeud.h
+++ noeud.h
@@ -1,18 +1,39 @@
 class noeud
 {
 
-	//méthodes
-	public: 
-		noeud(); //constructeur par défaut
-		noeud(noeud* noeud1, noeud* noeud2); //constructeur utilisé en cas de AND ou OR
-		noeud(noeud* noeud1); //constructeur utilisé en cas de modification de NOT
-		~noeud(); //destructeur
-
-	//attributs
 	private: 
+
 		int nb_aretes = 0;
-		bool* var = nullptr;
+		int* var = nullptr;
 		noeud* edges = nullptr;
-		bool res = 0;
+		bool* res = nullptr;
+
+	public: 
+
+		//====================================================================
+		//Constructeurs
+		//====================================================================
+		
+		//Pour les constructeurs, l'operation doit être précisée sous forme de 
+		//cstring (il s'agit de const char* op)
+		
+		//ctor par defaut
+		noeud(); 
+		//ctor avec deux variables
+		noeud(const char* op, int var1, int var2); 
+		//ctor avec deux noeuds
+		noeud(const char* op, noeud* noeud1, noeud* noeud2);
+		//ctor avec un noeud et une variable
+		noeud(const char* op, int var, noeud* noeud);
+		//ctor pour not avec une variable
+		noeud(const char* op, int var);
+		//ctor pour not avec un noeud
+		noeud(const char* op, noeud* noeud);
+
+		//====================================================================
+		//Destructeur
+		//====================================================================
+		
+		~noeud(); //destructeur
 
 };
