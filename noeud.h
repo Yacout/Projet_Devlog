5e94a39f1db533584b07193d7512ae51a69080fe
Vincent Le Goff <vb.legoff@free.fr>
2020-05-11 17:54:31 +0200

    Ajout attributs nécessaires

--- noeud.h
+++ noeud.h
@@ -1,13 +1,18 @@
-class noeud{
+class noeud
+{
 
 	//méthodes
-public : 
-	noeud(); //constructeur par défaut
-	noeud(noeud* noeud1, noeud* noeud2); //constructeur utilisé en cas de AND ou OR
-	noeud(noeud* noeud1); //constructeur utilisé en cas de modification de NOT
-	~noeud(); //destructeur
+	public: 
+		noeud(); //constructeur par défaut
+		noeud(noeud* noeud1, noeud* noeud2); //constructeur utilisé en cas de AND ou OR
+		noeud(noeud* noeud1); //constructeur utilisé en cas de modification de NOT
+		~noeud(); //destructeur
 
 	//attributs
-private : 
+	private: 
+		int nb_aretes = 0;
+		bool* var = nullptr;
+		noeud* edges = nullptr;
+		bool res = 0;
 
-}
+};
