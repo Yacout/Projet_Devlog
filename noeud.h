ca0ea09b848f60ef00818b94cb042e1150feb677
Vincent Le Goff <vb.legoff@free.fr>
2020-05-12 10:49:16 +0200

    const char* op -> int op

--- noeud.h
+++ noeud.h
@@ -16,29 +16,29 @@ class noeud
 		//====================================================================
 		
 		//Pour les constructeurs, l'operation doit être précisée sous forme d' 
-		//entier (il s'agit de int op):
+		//entier (il s'agit de int op_par):
 		//1 pour AND
-		//2 pour OR
+	//2 pour OR
 		//3 pour NOT
 		
 		//ctor par defaut
 		noeud(); 
 		//ctor avec deux variables
-		noeud(int op, int var1, int var2); 
+		noeud(int op_par, int var1, int var2); 
 		//ctor avec deux noeuds
-		noeud(int op, noeud* noeud1, noeud* noeud2);
+		noeud(int op_par, noeud* noeud1, noeud* noeud2);
 		//ctor avec un noeud et une variable
-		noeud(int op, int var, noeud* noeud);
+		noeud(int op_par, int var, noeud* noeud);
 		//ctor avec deux constantes
-		noeud(int op, bool const1, bool const2);
+		noeud(int op_par, bool const1, bool const2);
 		//ctor avec une constante et un noeud
-		noeud(int op, bool const1, noeud* noeud);
+		noeud(int op_par, bool const1, noeud* noeud);
 		//ctor avec une constante et une variable
-		noeud(int op, bool const1, int var1);
+		noeud(int op_par, bool const1, int var1);
 		//ctor pour not avec une variable
-		noeud(int op, int var);
+		noeud(int op_par, int var);
 		//ctor pour not avec un noeud
-		noeud(int op, noeud* noeud);
+		noeud(int op_par, noeud* noeud);
 
 		//====================================================================
 		//Destructeur
