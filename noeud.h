84f693f7c00bbce49c0d907757b1e69124e33147
Vincent Le Goff <vb.legoff@free.fr>
2020-05-12 10:16:00 +0200

    Ajout constructeurs pour constantes

--- noeud.h
+++ noeud.h
@@ -24,6 +24,12 @@ class noeud
 		noeud(const char* op, noeud* noeud1, noeud* noeud2);
 		//ctor avec un noeud et une variable
 		noeud(const char* op, int var, noeud* noeud);
+		//ctor avec deux constantes
+		noeud(const char* op, bool const1, bool const2);
+		//ctor avec une constante et un noeud
+		noeud(const char* op, bool const1, noeud* noeud);
+		//ctor avec une constante et une variable
+		noeud(const char* op, bool const1, int var1);
 		//ctor pour not avec une variable
 		noeud(const char* op, int var);
 		//ctor pour not avec un noeud
