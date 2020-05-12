74683df4fe975051ac9c8428bd2c0f958a9ca95b
Vincent Le Goff <vb.legoff@free.fr>
2020-05-12 10:36:51 +0200

    const char* op -> int op

--- noeud.h
+++ noeud.h
@@ -7,7 +7,7 @@ class noeud
 		int nb_aretes = 0;
 		int* var = nullptr; //tableau
 		bool* consts = nullptr; //tableau
-		noeud* edges = nullptr; //tableau
+		noeud* aretes = nullptr; //tableau
 
 	public: 
 
@@ -15,27 +15,30 @@ class noeud
 		//Constructeurs
 		//====================================================================
 		
-		//Pour les constructeurs, l'operation doit être précisée sous forme de 
-		//cstring (il s'agit de const char* op)
+		//Pour les constructeurs, l'operation doit être précisée sous forme d' 
+		//entier (il s'agit de int op):
+		//1 pour AND
+		//2 pour OR
+		//3 pour NOT
 		
 		//ctor par defaut
 		noeud(); 
 		//ctor avec deux variables
-		noeud(const char* op, int var1, int var2); 
+		noeud(int op, int var1, int var2); 
 		//ctor avec deux noeuds
-		noeud(const char* op, noeud* noeud1, noeud* noeud2);
+		noeud(int op, noeud* noeud1, noeud* noeud2);
 		//ctor avec un noeud et une variable
-		noeud(const char* op, int var, noeud* noeud);
+		noeud(int op, int var, noeud* noeud);
 		//ctor avec deux constantes
-		noeud(const char* op, bool const1, bool const2);
+		noeud(int op, bool const1, bool const2);
 		//ctor avec une constante et un noeud
-		noeud(const char* op, bool const1, noeud* noeud);
+		noeud(int op, bool const1, noeud* noeud);
 		//ctor avec une constante et une variable
-		noeud(const char* op, bool const1, int var1);
+		noeud(int op, bool const1, int var1);
 		//ctor pour not avec une variable
-		noeud(const char* op, int var);
+		noeud(int op, int var);
 		//ctor pour not avec un noeud
-		noeud(const char* op, noeud* noeud);
+		noeud(int op, noeud* noeud);
 
 		//====================================================================
 		//Destructeur
