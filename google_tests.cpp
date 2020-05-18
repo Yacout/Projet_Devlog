d416dd4bdfec21e4c78499c9e297811f269f47e3
Vincent Le Goff <vb.legoff@free.fr>
2020-05-18 16:22:06 +0200

    Ajout test pour size

--- google_tests.cpp
+++ google_tests.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "noeud.cpp"
 #include <iostream>
+
 TEST(GTestTests, ExampleTest){
 
 	bool list[2];
@@ -18,4 +19,20 @@ TEST(GTestTests, ExampleTest){
 	EXPECT_EQ(noeud5.compute(list), 1);
 };
 
+TEST(GTestTests, TestSize){
+
+	bool F = 0;
+	bool T = 1;
+
+	noeud noeud1(3, 1);
+	noeud noeud2(1, F, T);
+	noeud noeud3(1, true, 0);
+	noeud noeud4(2, &noeud1, &noeud2);
+	noeud noeud5(1, &noeud4, &noeud3);
+	
+	int s;
+	noeud5.size(s);
+
+	EXPECT_EQ(s, 5);
+};
 
