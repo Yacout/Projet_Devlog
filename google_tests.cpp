2f1309b592ec09cbf32c73a07b592558880c00d5
Vincent Le Goff <vb.legoff@free.fr>
2020-05-25 14:54:04 +0200

    Correction mineure

--- google_tests.cpp
+++ google_tests.cpp
@@ -55,14 +55,9 @@ TEST(GTestTests, TestListe){
 	liste_noeud[3] = new noeud(2, liste_noeud[0], liste_noeud[1]);
 	liste_noeud[4] = new noeud(1, liste_noeud[2], liste_noeud[3]);
 
-	noeud noeud1(1);
-	noeud noeud2(1, F, T);
-	noeud noeud3(1, true, 0);
-	noeud noeud4(2, &noeud1, &noeud2);
-	noeud noeud5(1, &noeud4, &noeud3);
 	noeud defaut(1, F, F);
 	int s=0;
-	noeud5.size(s);
+	liste_noeud[4]->size(s);
 	noeud* arr2[s];
 	
 	for(int i=0; i<s;i++){		//remplissage du tableau de pointeurs, pour savoir quelles parties du tableau ont été utilisés
